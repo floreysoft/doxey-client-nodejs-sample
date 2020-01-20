fb78ee015032fdf6076bb60d250fb4f714e4cfe2
Alexander Brickwedde <alex_git@brickwedde.de>
2020-01-20 19:07:24 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Sample JavaScript client for Doxey
+
+## run on your system
+
+This is a sample JavaScript client for the Doxey API. After you checked out, you can build and start it with
+```
+yarn
+
+yarn start
+```
+
+Then open a browser at http://127.0.0.1:8080/index.html
+
+You can either opload an ODT (LibreOffice/OpenOffice) file or enter an URL pointing at a Google Doc or ODT on Drive or other hosting services.
---
+++ main.js
@@ -0,0 +1,56 @@
+const url = require("url");
+const http = require("http");
+const doxeyclient = require("doxey-client-nodejs");
+
+const app = http.createServer((request, response) => {
+    var query = url.parse(request.url, true).query;
+
+    var model = {
+        "name": "John Doe",
+        "company": "ACME",
+        "address": "1234 Market Street, San Francisco, CA 94103, United States",
+        "date": "2019-09-23T18:25:00.000Z",
+        "invoiceNumber": "INV-12345678",
+        "signer": "Cora Nilson",
+        "signature": "https://www.doxey.io/images/signature.png",
+        "subtotal": 1301,
+        "vatRate": 19,
+        "vat": 247.19,
+        "total": 1548.19,
+        "items": [
+            {
+                "name": "Project Setup",
+                "description": "Create github repo, setup timetracker and Slack channels",
+                "amount": 90
+            },
+            {
+                "name": "Optimize Photos",
+                "description": "Scan, crop and scale images to reduce loading times",
+                "amount": 340.50
+            },
+            {
+                "name": "Website structure",
+                "description": "Copy blank boostrap template. Setup pages and adjust links",
+                "amount": 250.50
+            },
+            {
+                "name": "CSS theme",
+                "description": "Create CSS styles according to CI",
+                "amount": 620
+            }
+        ]
+    };
+
+    doxeyclient.mergeUrl ("api.doxey.io", 80, "/merge", "https://docs.google.com/document/d/1urL-JV2m85jry1_tatbjSFBjUZgGiMmwwNR9X8UTUTg/edit", "PDF", model, "de-DE", "GMT+01:00", "EUR", "")
+    .then((buffer) => {
+        response.writeHead(200, { "Content-Type": "application/pdf", "Content-Disposition": "inline" });
+        response.end(buffer, 'binary');
+    })
+    .catch(_ => {
+        response.writeHead(500, { "Content-Type": "text/plain" });
+        response.end("Error");
+    });
+});
+
+app.listen(3000);
+console.log ("Open a browser at http://127.0.0.1:3000 to create a PDF");
